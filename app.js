8278a68d41ab5ceffee5316438a7d1fa37783118
matt <matt@fiddaman.net>
2022-01-23 23:16:24 +0000

    Added session middleware

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
 
 // Import required modules
 const express = require('express');
+const session = require('express-session');
 
 // Import user defined modules
+const DatabaseConnectionPool = require('./lib/DatabaseConnectionPool');
 const importJSON = require('./lib/importJSON');
 
 async function main() {
-	// Import server config file
+	// Import config files
 	const serverOptions = importJSON('server');
+	const sessionOptions = importJSON('session');
 
-	// Initialise up express app
+	// Set up express-session to store in mysql database
+	const mysqlStore = require('express-mysql-session')(session);
+	const sessionStore =
+		new mysqlStore({},(await new DatabaseConnectionPool()).pool);
+
+	// Initialise express app
 	const app = express();
+
+	// Set up session middleware
+	app.use(session({
+		secret: sessionOptions.sessionSecret,
+		saveUninitialized: false,
+		resave: false,
+		store: sessionStore,
+		cookie: {
+			maxAge: sessionOptions.sessionLifetime,
+			sameSite: true
+		}
+	}));
 
 	// Start the server
 	app.listen(serverOptions.port, () => {

401eaa56e1bfc3e7a7d8c5be26afa86efa23c327
matt <matt@fiddaman.net>
2022-02-13 22:11:15 +0000

    Added route for registering an Account

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const router = express.Router();
 
+const Account = require('../lib/Account');
+const validator = require('../lib/validator');
+
 router.get('/', (req, res) => {
 	return res.render('index', {
 		title: 'Stratos - Home'
@@ -32,6 +35,49 @@ router.get('/logout', (req, res) => {
 		title: 'Stratos - Logout',
 		username: req.session.fullName
 	});
+});
+
+router.post('/register', async (req, res) => {
+	let fields;
+	try {
+		fields = validator.validate(req.body,
+			[
+				'fname',
+				'lname',
+				'email',
+				'password',
+				'confPassword'
+			],
+			{
+				email: 'email',
+				password: [ 'password', 'confPassword' ]
+			}
+		).fields;
+	} catch (e) {
+		console.error(e);
+		return res.status(400).json({ status: 'Invalid' });
+	}
+
+	let a;
+	try {
+		a = await Account.createAccount(
+			fields.get('fname'),
+			fields.get('onames'),
+			fields.get('lname'),
+			fields.get('email'),
+			fields.get('password')
+		);
+	} catch (e) {
+		console.error(e);
+		return res.render('error', {
+			code: 400,
+			msg: 'Unable to create account'
+		});
+	}
+
+	a.login(req);
+
+	return res.redirect('/login');
 });
 
 module.exports = {

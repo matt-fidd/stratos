e709d74f74b6e56e55ea4a161ff60cea41789787
matt <matt@fiddaman.net>
2022-02-24 15:47:24 +0000

    Moved data from view to route in preparation of db integration

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,59 @@ router.get('/dashboard', (req, res) => {
 	return res.render('dashboard', {
 		title: 'Stratos - Dashboard',
 		current: 'Dashboard',
-		name: req.session.fullName
+		name: req.session.fullName,
+		stats: [
+			{
+				value: 5,
+				text: 'Classes'
+			},
+			{
+				value: 11,
+				text: 'Completed Tests'
+			},
+			{
+				value: 1,
+				text: 'Upcoming Test'
+			},
+			{
+				value: '90%',
+				text: 'Pass rate'
+			}
+		],
+		recentTests: [
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			},
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			},
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			}
+		],
+		upcomingTests: [
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			},
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			},
+			{
+				id: 1,
+				date: '08/01/2022',
+				name: 'Lagged homework 8'
+			}
+		]
 	});
 });
 

959576585d1086131af870424dc6dcd07ae5401d
matt <matt@fiddaman.net>
2022-03-03 02:17:53 +0000

    Added class route, contains mostly real data but some test data too

--- routes/class.js
+++ routes/class.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
+const Class = require('../lib/Class');
 const User = require('../lib/User');
 
 router.get('/classes', async (req, res) => {
@@ -13,6 +14,47 @@ router.get('/classes', async (req, res) => {
 		current: 'Classes',
 		name: req.session.fullName,
 		classes: await u.getClasses()
+	});
+});
+
+router.get('/class/:id', async (req, res) => {
+	const c = await new Class(req.params.id);
+	const linkRoot = `/class/${c.id}`;
+
+	return res.render('class', {
+		title: `Stratos - Class - ${c.name}`,
+		current: 'Classes',
+		name: req.session.fullName,
+		className: c.name,
+		teachers: c.teachers,
+		members: c.students,
+		recentTests: await c.getTests({ range: 'before' }),
+		upcomingTests: await c.getTests({ range: 'after' }),
+		contactLink: `${linkRoot}/contact`,
+		testsLink: `${linkRoot}/tests`,
+		reportsLink: `${linkRoot}/reports`,
+		stats: [
+			{
+				value: 5,
+				text: 'Tests'
+			},
+			{
+				value: 3,
+				text: 'Tests completed'
+			},
+			{
+				value: 2,
+				text: 'Tests upcoming'
+			},
+			{
+				value: '72%',
+				text: 'Average percentage'
+			},
+			{
+				value: '50%',
+				text: 'Last percentage'
+			},
+		]
 	});
 });
 

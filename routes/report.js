fcde0790238799d7656f67135f3f1163b29e0efc
matt <matt@fiddaman.net>
2022-04-21 16:48:13 +0000

    Add route for reports page, with test data

--- routes/report.js
+++ routes/report.js
@@ -3,21 +3,56 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/reports', (req, res, next) => {
-	/* eslint-disable multiline-comment-style */
-	/*
+router.get('/reports', (req, res) => {
 	return res.render('reports', {
 		...req.hbsContext,
 		title: 'Stratos - Reports',
-		current: 'Reports'
+		current: 'Reports',
+		types: [
+			{
+				key: 'student',
+				value: 'Student'
+			},
+			{
+				key: 'class',
+				value: 'Class'
+			}
+		],
+		targets: JSON.stringify({
+			student: [
+				{
+					id: '1',
+					name: 'joe'
+				}
+			],
+			class: [
+				{
+					id: '1',
+					name: 'joeseph'
+				},
+				{
+					id: '2',
+					name: 'bob'
+				},
+				{
+					id: '3',
+					name: 'fred'
+				},
+				{
+					id: '4',
+					name: 'mike'
+				}
+			]
+		})
 	});
-	*/
-	/* eslint-enable multiline-comment-style */
+});
 
-	next();
+router.post('/report/generate', (req, res) => {
+	return res.redirect('/admin/reports');
 });
 
 module.exports = {
 	root: '/admin',
 	router: router
 };
+
